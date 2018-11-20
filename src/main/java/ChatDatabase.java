e76cebda4dfcc86e06cc7578d9d050a566babc08
Hossein Yousefi <u.hossein@yahoo.com>
2018-11-20 06:26:57 +0100

    first commit

---
+++ src/main/java/ChatDatabase.java
@@ -0,0 +1,36 @@
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+
+public class ChatDatabase {
+    MongoClient mongoClient;
+    MongoDatabase mongoDatabase;
+    MongoCollection mongoCollection;
+
+    static public final String DATABASE_NAME = "ChatDatabase";
+    static public final String COLLECTION_NAME = "chats";
+    static public final String USER_FIELD = "user";
+    static public final String CHAT_FIELD = "chat";
+
+    public ChatDatabase() {
+        mongoClient = new MongoClient();
+        mongoDatabase = mongoClient.getDatabase(DATABASE_NAME);
+        mongoCollection = mongoDatabase.getCollection(COLLECTION_NAME);
+        mongoCollection.drop(); // for testing
+    }
+
+    public FindIterable<Document> getChats() {
+        return mongoCollection.find();
+    }
+
+    public void addChat(String user, String chat) {
+        Document toBeAdded = new Document();
+        toBeAdded.put(USER_FIELD, user);
+        toBeAdded.put(CHAT_FIELD, chat);
+        mongoCollection.insertOne(toBeAdded);
+    }
+
+}
